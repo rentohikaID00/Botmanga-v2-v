b1ef2dcfa1fc8ec5d460d2ddf0f8729dcd43e0e9
rentohikaID00 <rentohika00@gmail.com>
2026-04-18 15:06:33 +0700

    Update translator.py

--- translator.py
+++ translator.py
@@ -6,14 +6,4 @@ from PIL import Image, ImageDraw, ImageFont
 from deep_translator import GoogleTranslator
 
 def process_manga_image(input_path, output_path):
-    img = cv2.imread(input_path)
-    if img is None:
-        return False
-
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
-
-    data = pytesseract.image_to_data(thresh, lang='jpn', output_type=Output.DICT)
-
-    pil_img = Image.fromarray(img)
-    draw
+    img
